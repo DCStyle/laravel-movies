d01c463f439b644fadfcc2f5457e187a0e935540
dcstylexf <dcstylexf@gmail.com>
2024-12-30 06:29:17 +0700

    Updated login redirect

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,13 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                if (auth()->user()->hasRole('admin')) {
+                    return redirect('/admin');
+                } elseif (auth()->user()->hasRole('mod')) {
+                    return redirect('/mod');
+                } else {
+                    return redirect(RouteServiceProvider::HOME);
+                }
             }
         }
 
